28206ecaacbf21a22949ee474c1e3e991c4bcdac
TheophileSimoes <theophile.sdn@gmail.com>
2024-11-06 16:57:49 +0100

    Adding the list that will allow the navigation by letter

---
+++ client/src/components/Liste.tsx
@@ -0,0 +1,44 @@
+const list = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+function Liste() {
+  return (
+    <>
+      <div>
+        {list.map((Alphabet) => (
+          <button type="button" key="Alphabet" onClick="">
+            {Alphabet}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Liste;
