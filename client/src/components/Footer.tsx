57a060ff56c6484757ea2a4c4d0d2d21e03f2558
Daniel Stacoffe <daniel.stacoffe@wanadoo.fr>
2024-11-12 11:50:19 +0100

    Creation et reorganisation footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,31 @@
+import "./Footer.css";
+
 function Footer() {
-  return <footer>F O O T E R</footer>;
+  return (
+    <footer>
+      <h3>Le saviez-vous ?</h3>
+      <p>
+        Que ce soit pour un rendez-vous d’affaires, un date ou juste pour boire
+        un verre avec des potes, les bars à cocktails de Lyon, tous plus riches
+        et divers les uns que les autres, vous ouvriront toujours leurs portes.
+        Pas question ici de vous abreuver de simples jus d’oranges ou de jus de
+        citrons auxquels on aurait maladroitement ajouté quelques centilitres de
+        vodka ou de tequila. On vous parle d’élixirs authentiques préparés avec
+        soin par des professionnels qui savent ce qu’ils font. Dégustez y les
+        cocktails classiques, tels que l’aperol spritz ou le blue lagoon, mais
+        découvrez surtout des créations originales et signatures qui valent le
+        détour. À base de vermouth lyonnais, de rhum blanc agricole, agrémentés
+        de sirop de sucre, de fleur de sel, de notes exotiques ou épicées,
+        toutes les combinaisons sont possibles. Tantôt subtiles, tantôt
+        intenses, sucrées, piquantes, voire envoûtantes, c’est une véritable
+        expérience que vous vous apprêtez à vivre en goûtant les saveurs qui
+        vous seront proposées par le barman. Chaque bar à cocktail a sa propre
+        identité. Soyez transportés dans les années 20, dans les bars de la
+        Nouvelle Orléans des années 70, dans un cocon asiatique, les voyages
+        sont divers mais se regroupent tous en une seule destination : LYON.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
