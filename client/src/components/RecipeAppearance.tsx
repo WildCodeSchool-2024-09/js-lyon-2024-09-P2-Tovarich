4603a11484ee6505b112e5f3f461759eee51e29f
DameDanny <161458137+DameDanny@users.noreply.github.com>
2024-11-13 16:50:01 +0100

    Creation of recipe appearance

---
+++ client/src/components/RecipeAppearance.tsx
@@ -0,0 +1,79 @@
+import "./RecipeAppearance.css";
+
+interface cocktailPros {
+  idDrink: string;
+  strDrink: string;
+  strCategory: string;
+  strAlcoholic: string;
+  strGlass: string;
+  strInstructions: string;
+  strDrinkThumb: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strMeasure9: string;
+  strMeasure10: string;
+  strMeasure11: string;
+  strMeasure12: string;
+  strMeasure13: string;
+  strMeasure14: string;
+  strMeasure15: string;
+  strImageSource: string;
+  strImageAttribution: string;
+}
+
+interface cocktailProps {
+  recipeData: cocktailPros[];
+}
+
+function RecipeAppearance({ recipeData }: cocktailProps) {
+  // const list: JSX.Element[] = [];
+  // const fonctionRecette = (data: cocktailProps) => {
+  //   return list;
+  // };
+
+  return (
+    <>
+      <div>
+        {recipeData.map((recipeDetail) => (
+          <article className="infoRecipe" key={recipeDetail.idDrink}>
+            <h2>Recipe of the {recipeDetail.strDrink}</h2>
+            <section className="image">
+              <img
+                src={recipeDetail.strDrinkThumb}
+                alt={recipeDetail.strDrink}
+              />
+            </section>
+            <section className="instructions">
+              <ul>Ingredients</ul>
+              <h3>Instructions to recreate {recipeDetail.strDrink}</h3>
+              <p>{recipeDetail.strInstructions}</p>
+            </section>
+          </article>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default RecipeAppearance;
