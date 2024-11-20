d853cec72411fa5738f3bce5731743a9a1083f93
Daniel Stacoffe <daniel.stacoffe@wanadoo.fr>
2024-11-20 12:25:02 +0100

    Ajout des deux boutons de filtres

--- client/src/components/CocktailButton.tsx
+++ client/src/components/CocktailButton.tsx
@@ -1,5 +1,39 @@
 import "./CocktailButton.css";
 
-function CocktailButton() {}
+interface alcoolProps {
+  alcool: boolean;
+  setAlcool: (alcool: boolean) => void;
+  noAlcool: boolean;
+  setNoAlcool: (noAlcool: boolean) => void;
+}
+function CocktailButton({
+  setAlcool,
+  setNoAlcool,
+  alcool,
+  noAlcool,
+}: alcoolProps) {
+  return (
+    <>
+      <div className="alcohol">
+        <button
+          className="alcoolButton"
+          type="button"
+          key="alcool"
+          onClick={() => setAlcool(!alcool)}
+        >
+          Alcoholic
+        </button>
+        <button
+          className="alcoolButton"
+          type="button"
+          key="Non alcool"
+          onClick={() => setNoAlcool(!noAlcool)}
+        >
+          Non Alcoholic
+        </button>
+      </div>
+    </>
+  );
+}
 
 export default CocktailButton;
